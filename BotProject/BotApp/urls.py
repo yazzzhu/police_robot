0baa4d1402f2245b12f33abeeb6cd9699df94228
yazzzhu <a0962072757@gmail.com>
2024-03-19 15:58:10 +0800

    line_bot

--- BotProject/BotApp/urls.py
+++ BotProject/BotApp/urls.py
@@ -1,8 +1,8 @@
-#from django.urls import path
-from django.conf.urls import url
+from django.urls import path
+#from django.conf.urls import url
 from . import views
  
 urlpatterns = [
-    #path('callback', views.callback),
-    url('callback/', views.callback),
+    path('callback', views.callback),
+    #url('callback/', views.callback),
 ]
