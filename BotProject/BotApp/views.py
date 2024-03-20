155e4d92e6df0bdb4b132374559ad91f930495e3
yazzzhu <a0962072757@gmail.com>
2024-03-20 11:24:26 +0800

    line_bot

--- BotProject/BotApp/views.py
+++ BotProject/BotApp/views.py
@@ -1,4 +1,6 @@
-# import 必要的函式庫
+from django.shortcuts import render
+
+# Create your views here.
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
@@ -6,13 +8,12 @@ from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
-# 這邊是Linebot的授權TOKEN(等等註冊LineDeveloper帳號會取得)，我們為DEMO方便暫時存在settings裡面存取，實際上使用的時候記得設成環境變數，不要公開在程式碼裡喔！
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def callback(request):
-
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
@@ -26,10 +27,11 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
-                line_bot_api.reply_message(
-                    event.reply_token,
-                   TextSendMessage(text=event.message.text)
-                )
+                mtext=event.message.text
+                message=[]
+                message.append(TextSendMessage(text=mtext))
+                line_bot_api.reply_message(event.reply_token,message)
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
