ac8ff47a4d4b9e4b1b0db01d75696464620d4964
yazzzhu <a0962072757@gmail.com>
2024-05-02 17:00:02 +0800

    line_bot

--- BotProject/BotProject/wsgi.py
+++ BotProject/BotProject/wsgi.py
@@ -14,19 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BotProject.settings')
 
 application = get_wsgi_application()
-
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-import urllib
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/15')
-def scheduled_job():
-    url = "https://github.com/yazzzhu/police_robot"
-    conn = urllib.request.urlopen(url)
-        
-    for key, value in conn.getheaders():
-        print(key, value)
-
-sched.start()
