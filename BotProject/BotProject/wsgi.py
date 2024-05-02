ad4cddc7e36b90a82207ce1b83aa344110f8b80a
yazzzhu <a0962072757@gmail.com>
2024-05-02 16:57:12 +0800

    line_bot

--- BotProject/BotProject/wsgi.py
+++ BotProject/BotProject/wsgi.py
@@ -17,14 +17,16 @@ application = get_wsgi_application()
 
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-import requests
+import urllib
 
 sched = BlockingScheduler()
 
-# 防止睡眠
-def DoNotSleep():
-    url = "https://police-robot.onrender.com/callback"
-    r = requests.get(url)
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/15')
+def scheduled_job():
+    url = "https://github.com/yazzzhu/police_robot"
+    conn = urllib.request.urlopen(url)
+        
+    for key, value in conn.getheaders():
+        print(key, value)
 
-# 防止自動休眠
-sched.add_job(DoNotSleep, trigger='interval', id='doNotSleeps_job', day_of_week='mon-fri', minute='*/15')
+sched.start()
--- clock.py
+++ clock.py
@@ -1,12 +1,24 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import requests
+import urllib
 
 sched = BlockingScheduler()
 
+'''
 # 防止睡眠
 def DoNotSleep():
-    url = "https://github.com/yazzzhu/police_robot"
+    url = "https://github.com/yazzzhu/police_robot" #https://police-robot.onrender.com
     r = requests.get(url)
 
 # 防止自動休眠
-sched.add_job(DoNotSleep, trigger='interval', id='doNotSleeps_job', day_of_week='mon-fri', minute='*/15')
+sched.add_job(conn, trigger='interval', id='doNotSleeps_job', day_of_week='mon-fri', minute='*/15')
+'''
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/15')
+def scheduled_job():
+    url = "https://github.com/yazzzhu/police_robot"
+    conn = urllib.request.urlopen(url)
+        
+    for key, value in conn.getheaders():
+        print(key, value)
+
+sched.start()
