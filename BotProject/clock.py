13c546ce733492ad9f78efca4cd82fee7a02e447
yazzzhu <a0962072757@gmail.com>
2024-05-02 16:23:35 +0800

    line_bot

---
+++ BotProject/clock.py
@@ -0,0 +1,12 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+
+sched = BlockingScheduler()
+
+# 防止睡眠
+def DoNotSleep():
+    url = "https://police-robot.onrender.com/callback"
+    r = requests.get(url)
+
+# 防止自動休眠
+sched.add_job(DoNotSleep, trigger='interval', id='doNotSleeps_job', day_of_week='mon-fri', minute='*/20')
